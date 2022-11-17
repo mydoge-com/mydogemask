1a5fc13b32d06eb2f3b1721268243e38cb4923cc
Alex Lewis <alexvlewis@gmail.com>
2022-11-17 03:33:50 -0800

    Order imports

--- src/views/Popup/App.js
+++ src/views/Popup/App.js
@@ -2,8 +2,8 @@ import { Box } from 'native-base';
 import { Route } from 'wouter';
 
 import { AppProvider } from '../../components/AppProvider';
-import Home from '../Home/App';
 import { useStorage } from '../../hooks/useStorage';
+import Home from '../Home/App';
 import { PasswordScreen } from './PasswordScreen';
 import { WalletScreen } from './WalletScreen';
 
@@ -11,6 +11,7 @@ function App() {
   const {
     storage: { isAuthenticated },
   } = useStorage();
+
   return (
     <AppProvider>
       <Route path='/'>
