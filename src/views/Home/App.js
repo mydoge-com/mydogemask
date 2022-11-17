615a897631d12cecf98d867ea4b4acc3009dbaaa
Tolu Lawson <lawsontolu1@gmail.com>
2022-11-16 21:50:03 -0800

    Add onboarding screen

--- src/views/Home/App.js
+++ src/views/Home/App.js
@@ -1,11 +1,42 @@
-import { Box } from 'native-base';
+import { Box, Image } from 'native-base';
 
 import { AppProvider } from '../../components/AppProvider';
 
 function App() {
   return (
     <AppProvider>
-      <Box>Home page (onboarding) - Create/import wallet</Box>
+      <Box h='100vh' overflow='hidden'>
+        <Box
+          w='50vw'
+          h='100%'
+          justifyContent='center'
+          alignItems='center'
+          bg='white'
+        >
+          <Image
+            source={{ uri: '/assets/bg.png' }}
+            h='100%'
+            position='absolute'
+            zIndex={-1}
+            left={0}
+            right={0}
+          />
+          <Image
+            source={{ uri: '/assets/mydoge-mask.png' }}
+            size={200}
+            resizeMode='contain'
+            position='absolute'
+            top='0px'
+            alignSelf='center'
+          />
+          <Image
+            source={{ uri: '/assets/intro.png' }}
+            size={500}
+            resizeMode='contain'
+          />
+        </Box>
+        <Box w='50vw' h='100%' />
+      </Box>
     </AppProvider>
   );
 }
--- src/views/Popup/App.js
+++ src/views/Popup/App.js
@@ -1,13 +1,20 @@
 import { Box } from 'native-base';
+import { Route } from 'wouter';
 
 import { AppProvider } from '../../components/AppProvider';
+import Home from '../Home/App';
 
 function App() {
   return (
     <AppProvider>
-      <Box width='360px' py='100px'>
-        Welcome to MyDoge
-      </Box>
+      <Route path='/'>
+        <Box width='360px' py='100px'>
+          Welcome to MyDoge
+        </Box>
+      </Route>
+      <Route path='/home'>
+        <Home />
+      </Route>
     </AppProvider>
   );
 }
