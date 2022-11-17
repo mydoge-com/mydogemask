c1634701102b6d5e27b2c5bc95efe5c1ea5cfe51
Tolu Lawson <lawsontolu1@gmail.com>
2022-11-17 12:45:11 -0800

    Improve spa routing

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import { createRoot } from 'react-dom/client';
 import { Route } from 'wouter';
 
 import Home from './views/Home/App';
+import Notification from './views/Notification/App';
+import Options from './views/Options/App';
 import Popup from './views/Popup/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <Route path='/' component={Popup}>
-      About Us
-    </Route>
-    <Route path='/home' component={Home}>
-      About Us
-    </Route>
+    <Route path='/' component={Popup} />
+    <Route path='/home' component={Home} />
+    <Route path='/options' component={Options} />
+    <Route path='/notification' component={Notification} />
   </React.StrictMode>
 );
