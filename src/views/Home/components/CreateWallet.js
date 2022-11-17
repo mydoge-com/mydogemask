b75b1c41d6f535cd7dcbcd97d9d1484a415c0d1f
Tolu Lawson <lawsontolu1@gmail.com>
2022-11-17 02:49:56 -0800

    Add create password screen

--- src/views/Home/components/CreateWallet.js
+++ src/views/Home/components/CreateWallet.js
@@ -1,5 +1,14 @@
-import { Link, Text, VStack } from 'native-base';
-import { useCallback } from 'react';
+import {
+  HStack,
+  Icon,
+  IconButton,
+  Input,
+  Link,
+  Text,
+  VStack,
+} from 'native-base';
+import { useCallback, useState } from 'react';
+import { FaAngleLeft, FaEye, FaEyeSlash } from 'react-icons/fa';
 
 import { BigButton } from './Button';
 
@@ -8,8 +17,33 @@ export const CreateWallet = ({ setScreen }) => {
     setScreen(2);
   }, [setScreen]);
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = useCallback(() => {
+    setShowPassword((current) => !current);
+  }, []);
+
+  const onBack = useCallback(() => {
+    setScreen('intro');
+  }, [setScreen]);
+
   return (
     <VStack px='15%' justifyContent='center' h='100%'>
+      <HStack alignItems='center' pb='30px'>
+        <IconButton
+          icon={<Icon as={FaAngleLeft} />}
+          rounded='full'
+          alignSelf='flex-start'
+          borderWidth='1px'
+          borderColor='gray.500'
+          color='gray.500'
+          size='24px'
+          onPress={onBack}
+        />
+        <Text ml='6px' color='gray.500'>
+          Back
+        </Text>
+      </HStack>
       <VStack bg='white' py='40px' rounded='sm' px='40px'>
         <Text fontSize='2xl'>
           Create a <Text fontWeight='bold'>Password</Text>
@@ -17,6 +51,47 @@ export const CreateWallet = ({ setScreen }) => {
         <Text color='gray.500' fontSize='14px'>
           You will need this password to access your wallet
         </Text>
+        <VStack py='40px'>
+          <Input
+            variant='filled'
+            placeholder='Enter Password'
+            py='14px'
+            type={showPassword ? 'text' : 'password'}
+            focusOutlineColor='brandYellow.500'
+            _hover={{
+              borderColor: 'brandYellow.500',
+            }}
+            InputRightElement={
+              <IconButton
+                icon={
+                  showPassword ? <Icon as={FaEye} /> : <Icon as={FaEyeSlash} />
+                }
+                onPress={toggleShowPassword}
+                color='gray.500'
+              />
+            }
+          />
+          <Input
+            variant='filled'
+            placeholder='Confirm Password'
+            py='14px'
+            mt='16px'
+            type={showPassword ? 'text' : 'password'}
+            focusOutlineColor='brandYellow.500'
+            _hover={{
+              borderColor: 'brandYellow.500',
+            }}
+            InputRightElement={
+              <IconButton
+                icon={
+                  showPassword ? <Icon as={FaEye} /> : <Icon as={FaEyeSlash} />
+                }
+                onPress={toggleShowPassword}
+                color='gray.500'
+              />
+            }
+          />
+        </VStack>
         <BigButton mt='20px' onPress={onCreateWallet}>
           Create Password
         </BigButton>
