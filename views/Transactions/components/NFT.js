231cae940cb74e12c9a61cded5adf825c0bcee18
Tolu Lawson <55362043+tolulawson@users.noreply.github.com>
2024-03-19 04:10:01 -0700

    Add NFT card

--- views/Transactions/components/NFT.js
+++ views/Transactions/components/NFT.js
@@ -1,22 +1,31 @@
-import { Box, Pressable } from 'native-base';
+import dayjs from 'dayjs';
+import { Box, Pressable, Text, VStack } from 'native-base';
 import { Fragment, useState } from 'react';
 
-export const NFT = ({ nft: { content, inscriptionId, preview } }) => {
+export const NFT = ({ nft: { content, inscriptionNumber, timestamp } }) => {
   const [, setIsOpen] = useState(false);
   return (
-    <Fragment key={inscriptionId}>
-      <Pressable onPress={() => setIsOpen(true)} paddingTop='10px'>
-        {/* <Text>{content}</Text> */}
-        <Box width='100%' borderRadius='12px' overflow='hidden'>
-          <img
-            // source={{ uri: preview }}
-            src={content}
-            width='100%'
-            height='auto'
-            alt='NFT'
-            resizeMode='contain'
-          />
-        </Box>
+    <Fragment key={inscriptionNumber}>
+      <Pressable onPress={() => setIsOpen(true)} paddingTop='20px'>
+        <VStack p='10px' borderRadius='22px' bg='gray.200'>
+          <Box width='100%' borderRadius='12px' overflow='hidden'>
+            <img
+              src={content}
+              width='100%'
+              height='auto'
+              alt='NFT'
+              resizeMode='contain'
+            />
+          </Box>
+
+          <Text fontSize='16px' fontWeight='bold' color='yellow.600' pt='10px'>
+            # {inscriptionNumber}
+          </Text>
+
+          <Text fontSize='12px' fontWeight='medium' color='gray.500'>
+            {dayjs(timestamp * 1000).format('YYYY-MM-DD HH:mm:ss')}
+          </Text>
+        </VStack>
       </Pressable>
     </Fragment>
   );
