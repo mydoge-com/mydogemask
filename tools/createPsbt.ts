033d366f65478b5d6db0c2b61c19fbbe45a53434
Alex Lewis <alexvlewis@gmail.com>
2024-08-17 16:37:16 -0700

    Remove nownodes from tools and convert createPsbt to typescript

--- tools/createPsbt.ts
+++ tools/createPsbt.ts
@@ -4,9 +4,9 @@ const sb = require('satoshi-bitcoin');
 
 dotenv.config();
 
-const api = require('./nownodes');
+const api = require('../scripts/api');
 
-const { nownodes } = api;
+const { mydoge } = api;
 
 const network = {
   messagePrefix: '\x19Dogecoin Signed Message:\n',
@@ -44,20 +44,24 @@ async function run() {
 
   console.log('initialized keypairs, amount, fee');
 
-  const utxos1 = (await nownodes.get(`utxo/${changeAddress}`)).data.sort(
-    (a, b) => {
-      const aValue = Number(a.value);
-      const bValue = Number(b.value);
-      return bValue > aValue ? 1 : bValue < aValue ? -1 : a.height - b.height;
-    }
-  );
-  const utxos2 = (await nownodes.get(`utxo/${signerAddress}`)).data.sort(
-    (a, b) => {
-      const aValue = Number(a.value);
-      const bValue = Number(b.value);
-      return bValue > aValue ? 1 : bValue < aValue ? -1 : a.height - b.height;
-    }
-  );
+  const utxos1 = (
+    await mydoge.get('/wallet/info', {
+      params: { route: `utxo/${changeAddress}` },
+    })
+  ).data.sort((a, b) => {
+    const aValue = Number(a.value);
+    const bValue = Number(b.value);
+    return bValue > aValue ? 1 : bValue < aValue ? -1 : a.height - b.height;
+  });
+  const utxos2 = (
+    await mydoge.get('/wallet/info', {
+      params: { route: `utxo/${signerAddress}` },
+    })
+  ).data.sort((a, b) => {
+    const aValue = Number(a.value);
+    const bValue = Number(b.value);
+    return bValue > aValue ? 1 : bValue < aValue ? -1 : a.height - b.height;
+  });
 
   if (utxos1.length === 0) {
     throw new Error('no utxos for address 1');
@@ -81,9 +85,15 @@ async function run() {
   const index1 = utxos1[0].vout;
   const index2 = utxos2[0].vout;
   const index3 = utxos2[1].vout;
-  const tx1 = await nownodes.get(`tx/${utxos1[0].txid}`);
-  const tx2 = await nownodes.get(`tx/${utxos2[0].txid}`);
-  const tx3 = await nownodes.get(`tx/${utxos2[1].txid}`);
+  const tx1 = await mydoge.get('/wallet/info', {
+    params: { route: `tx/${utxos1[0].txid}` },
+  });
+  const tx2 = await mydoge.get('/wallet/info', {
+    params: { route: `tx/${utxos2[0].txid}` },
+  });
+  const tx3 = await mydoge.get('/wallet/info', {
+    params: { route: `tx/${utxos2[1].txid}` },
+  });
   const value1 = sb.toBitcoin(tx1.data.vout[index1].value);
   const value2 = sb.toBitcoin(tx2.data.vout[index2].value);
   const value3 = sb.toBitcoin(tx3.data.vout[index3].value);
