a0d81108e3126f1fff916db28923afe5e161eff5
Alex Lewis <alexvlewis@gmail.com>
2024-08-17 16:37:50 -0700

    Remove nownodes from api.js

--- scripts/api.js
+++ scripts/api.js
@@ -8,10 +8,7 @@ import {
   DOGINALS_WALLET_API_URL,
   DOGINALS_WALLET_API_V2_URL,
   MYDOGE_BASE_URL,
-  NOWNODES_BASE_URL,
 } from './helpers/constants';
-
-export const apiKey = process.env.NEXT_PUBLIC_NOWNODES_API_KEY;
 
 const retryOptions = retry({
   delayTimer: 500,
@@ -22,13 +19,6 @@ const retryOptions = retry({
   retryOnNetworkError: false,
   resolveWithLatestResponse: false,
   skip: (_, opts) => opts.method !== 'GET',
-});
-
-export const nownodes = wretch(NOWNODES_BASE_URL, {
-  headers: {
-    'api-key': apiKey,
-  },
-  redirect: 'follow',
 });
 
 export const doginals = wretch(DOGINALS_WALLET_API_URL).middlewares([
